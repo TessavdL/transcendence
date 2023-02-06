c9562e0c4974f093221f895a624ca6ca5092a3e6
Tessa Van der loo <tevan-de@f1r2s6.codam.nl>
2023-02-06 14:42:38 +0100

    pushed new main to main branch

---
+++ back_end/app/src/messages/messages.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+	messages: Message[] = [];
+	clientToUser = {};
+
+	identify(name: string, clientId: string) {
+		this.clientToUser[clientId] = name;
+		return Object.values(this.clientToUser);
+	}
+
+	getClientName(clientId: string) {
+		return this.clientToUser[clientId];
+	}
+	
+	create(createMessageDto: CreateMessageDto) {
+		const message = {...createMessageDto};
+		this.messages.push(createMessageDto);
+		console.log(message);
+		return message;
+	}
+
+	findAll() {
+		return this.messages;
+	}
+
+	join() {
+		return 'This action joins a room';
+	}
+
+	typing() {
+		return 'This action shows if the user is typing';
+	}
+}
