a5195f507287c3881fcbab5d5f3aa81b71dd0f2b
Jelle riense Van der kooi <jelvan-d@f1r2s4.codam.nl>
2023-02-23 18:26:19 +0100

    feature: started adding userList functionality

--- back_end/app/src/user/user.controller.ts
+++ back_end/app/src/user/user.controller.ts
@@ -2,15 +2,25 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/guards';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-	constructor (private prisma: PrismaService) {}
+	constructor (private userService: UserService, private prisma: PrismaService) {}
 
 	@UseGuards(JwtAuthGuard)
 	@Get('/')
 	getUser(@Req() request): User {
 		const user: User = request.user;
 		return (user);
+	}
+
+	@UseGuards(JwtAuthGuard)
+	@Get('userlist')
+	getUserList(@Req() request) {
+		const user: User = request.user;
+		// console.log(user);
+		// console.log("going to service");
+		return (this.userService.getUserList(user));
 	}
 }
--- back_end/app/src/user/user.service.ts
+++ back_end/app/src/user/user.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-	
+	constructor(private prisma: PrismaService) {}
+
+	async getUserList(user: User) {
+		const userlist: User[] = await this.prisma.user.findMany();
+
+		console.log(userlist);
+		return (userlist);
+	}
 }
