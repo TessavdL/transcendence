71e4b36ef38616d5e3d66398af5172d8eaa9d3c1
Tessa Van der loo <tevan-de@f1r2s6.codam.nl>
2023-03-14 16:31:29 +0100

    #37 added endpoint to retrieve userElement based on intraId, shows relations of active user with the other user based on their intraId

--- back_end/app/src/user/user.controller.ts
+++ back_end/app/src/user/user.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { JwtAuthGuard } from 'src/auth/guards';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { userElement } from './types';
+import { UserElement } from './types';
 import { UserService } from './user.service';
 
 @UseGuards(JwtAuthGuard)
@@ -16,12 +16,19 @@ export class UserController {
 	}
 
 	@Get('users')
-	returnUserElements(@Req() request): Promise<userElement[]> {
+	returnUserElements(@Req() request): Promise<UserElement[]> {
 		return (this.userService.getUserElements(request.user));
 	}
 
 	@Get('usersexceptself')
 	getUserListExceptSelf(@Req() request): Promise<User[]> {
 		return (this.userService.getUserListExceptSelf(request.user));
+	}
+
+	@Get(':id')
+	getUserElementBasedOnIntraId(@Req() request, @Param() params): Promise<UserElement> {
+		const user: User = request.user;
+		const intraId: number = parseInt(params.id);
+		return (this.userService.getUserElementBasedOnIntraId(user, intraId));
 	}
 }
