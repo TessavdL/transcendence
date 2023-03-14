39768092b588077fb31438071edae8b01c7a7e70
Tessa Van der loo <tevan-de@f1r2s6.codam.nl>
2023-03-14 15:53:45 +0100

    #43 pleaced guard on top of controller decorator so the guard is applied for all endpoints in controller

--- back_end/app/src/user/user.controller.ts
+++ back_end/app/src/user/user.controller.ts
@@ -5,23 +5,21 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { userElement } from './types';
 import { UserService } from './user.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
-	constructor (private userService: UserService, private prisma: PrismaService) {}
+	constructor(private userService: UserService, private prisma: PrismaService) { }
 
-	@UseGuards(JwtAuthGuard)
 	@Get('/')
 	getUser(@Req() request): User {
 		return (request.user);
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get('users')
 	returnUserElements(@Req() request): Promise<userElement[]> {
 		return (this.userService.getUserElements(request.user));
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get('usersexceptself')
 	getUserListExceptSelf(@Req() request): Promise<User[]> {
 		return (this.userService.getUserListExceptSelf(request.user));
