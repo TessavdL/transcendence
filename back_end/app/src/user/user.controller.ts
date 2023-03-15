2d933bc78a1d1903cff8deaebbbde3cb56193a6b
Tessa van der Loo <tessavdl331@github.com>
2023-03-15 17:47:17 +0100

    37 cleanup: removed jwtauthguard from functions as it is already on top of the controller

--- back_end/app/src/user/user.controller.ts
+++ back_end/app/src/user/user.controller.ts
@@ -4,7 +4,6 @@ import { JwtAuthGuard } from 'src/auth/guards';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { OtherUserIntraDto } from './dto/other-user-intra.dto';
 import { UserElement } from './types';
-
 import { UserService } from './user.service';
 
 @UseGuards(JwtAuthGuard)
@@ -22,19 +21,16 @@ export class UserController {
 		return (this.userService.getUserElements(request.user));
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Post('block_user')
 	blockUser(@Req() request, @Body() OtherUserIntraDto: OtherUserIntraDto) {
 		return (this.userService.blockUser(request.user, OtherUserIntraDto.otherIntraId));
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Post('unblock_user')
 	unblockUser(@Req() request, @Body() OtherUserIntraDto: OtherUserIntraDto) {
 		return (this.userService.unblockUser(request.user, OtherUserIntraDto.otherIntraId));
 	}
 
-	@UseGuards(JwtAuthGuard)
 	@Get('usersexceptself')
 	getUserListExceptSelf(@Req() request): Promise<User[]> {
 		return (this.userService.getUserListExceptSelf(request.user));
@@ -43,9 +39,9 @@ export class UserController {
 	@Get('createdummy')
 	async createDummyUser(): Promise<void> {
 		return (this.userService.createDummyUser());
-    }
+	}
 
-    @Get(':id')
+	@Get(':id')
 	getUserElementBasedOnIntraId(@Req() request, @Param() params): Promise<UserElement> {
 		const user: User = request.user;
 		const otherIntraId: number = parseInt(params.id);
