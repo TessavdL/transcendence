8fef7ab62a3fc016236ac927ad13e8cdd8b1f566
Hz Lin <hzlin@Tora-neko.local>
2023-03-15 15:09:32 +0100

    filter users card based the side bar nav

--- front_end/app/src/views/UsersView.vue
+++ front_end/app/src/views/UsersView.vue
@@ -6,29 +6,36 @@
                 <div class="col-sm-3 col-md-2 users-sidebar">
                     <ul class="nav flex-column">
                         <li class="nav-item">
-                            <h3 class="nav-link active" aria-current="page" href="#">
+                            <h3 class="nav-link"  @click="handleFilter('none')">
                                 <i class="bi bi-people-fill fs-4" style="font-size: 2rem; color: #ffffff;"></i>
                                 All Users</h3>
                         </li>
                         <li class="nav-item">
-                            <h3 class="nav-link" href="#">
+                            <h3 class="nav-link" @click="handleFilter('activityStatus')">
+                                <i class="bi bi-person-up fs-4" style="font-size: 2rem; color: #ffffff;"></i>
+                                Online</h3>
+                        </li>
+                        <li class="nav-item">
+                            <h3 class="nav-link" @click="handleFilter('friendStatus')">
                                 <i class="bi bi-person-heart fs-4" style="font-size: 2rem; color: #ffffff;"></i>
                                 Friends</h3>
                         </li>
                         <li class="nav-item">
-                            <h3 class="nav-link" href="#">
+                            <h3 class="nav-link" @click="handleFilter('blockedState')">
                                 <i class="bi bi-person-fill-slash fs-4" style="font-size: 2rem; color: #ffffff;"></i>
                                 Blocked</h3>
                         </li>
                     </ul>
+
                 </div>
                     
                 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 users-main">
                     <div class="card-group">
-                        <div v-for="user in users" :key="user.username">
+                        <div v-for="user in filteredUsers" :key="user.username">
                             <UserInfoCard :user="user" />
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -37,14 +44,17 @@
 
 <script setup lang="ts">
 import axios from "axios";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 
 import UserInfoCard from '@/components/UserInfoCard.vue';
 
 const users = ref([]);
+const filteredUsers= ref([]);
+const filterCat = ref<String>('none');
 
 onMounted(async () => {
     await getUsers();
+    filteredUsers.value = [...users.value];
 });
 
 async function getUsers() {
@@ -59,6 +69,36 @@ async function getUsers() {
             console.log("cannot get users infomation");
         });
 };
+
+const handleFilter = (cat: String) => {
+    filterCat.value = cat;
+    filteredUsers.value = [...users.value];
+    if (cat === 'activityStatus') {
+        filteredUsers.value = filteredUsers.value.filter((user) => {
+            return (
+                user.activityStatus === "ONLINE"
+            );
+        });;
+    }
+    else if (cat === 'friendStatus') {
+        filteredUsers.value = filteredUsers.value.filter((user) => {
+            return (
+                user.friendStatus === "FRIENDS"
+            );
+        });;
+    }
+    else if (cat === 'blockedState') {
+        filteredUsers.value = filteredUsers.value.filter((user) => {
+            return (
+                user.blockedState === true
+            );
+        });;
+    }
+    else {
+        filteredUsers.value = filteredUsers.value;
+    }
+}
+
 
 </script>
 
