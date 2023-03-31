aefb25e11cb628c52e5b106cbb08e89a6709963d
Tessa Van der loo <tevan-de@f1r2s6.codam.nl>
2023-03-31 17:17:27 +0200

    save testing environment, localhost:5173/chat2

--- front_end/app/src/router/index.ts
+++ front_end/app/src/router/index.ts
@@ -4,6 +4,7 @@ import storeUser from "@/store";
 import HomeView from '@/views/HomeView.vue';
 import GameView from "@/views/GameView.vue";
 import ChatView from "@/views/ChatView.vue";
+import ChatView2 from "@/views/ChatView2.vue";
 import ProfileCurrentView from "@/views/ProfileCurrentView.vue";
 import ProfileOtherView from "@/views/ProfileOtherView.vue";
 import ProfileEditView from "@/views/ProfileEditView.vue";
@@ -12,75 +13,80 @@ import LogoutView from "@/views/LogoutView.vue";
 import UsersView from "@/views/UsersView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: HomeView
-    },
-    {
-      path: '/game',
-      name: 'Game',
-      component: GameView
-    },
-    {
-      path: '/chat',
-      name: 'Chat',
-      component: ChatView
-    },
-    {
-      path: '/profile/current',
-      name: 'ProfileCurrent',
-      component: ProfileCurrentView,
-    },
-    {
-      path: '/profile/other/:intraId',
-      name: 'ProfileOther',
-      component: ProfileOtherView,
-      props: true,
-    },
-    {
-      path: '/profileedit',
-      name: 'ProfileEdit',
-      component: ProfileEditView,
-    },
-    {
-      path: '/users',
-      name: 'Users',
-      component: UsersView
-    },
-    {
-      path: '/logout',
-      name: 'Logout',
-      component: LogoutView
-    },
-  ]
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: HomeView
+		},
+		{
+			path: '/game',
+			name: 'Game',
+			component: GameView
+		},
+		{
+			path: '/chat',
+			name: 'Chat',
+			component: ChatView
+		},
+		{
+			path: '/chat2',
+			name: 'Chat2',
+			component: ChatView2
+		},
+		{
+			path: '/profile/current',
+			name: 'ProfileCurrent',
+			component: ProfileCurrentView,
+		},
+		{
+			path: '/profile/other/:intraId',
+			name: 'ProfileOther',
+			component: ProfileOtherView,
+			props: true,
+		},
+		{
+			path: '/profileedit',
+			name: 'ProfileEdit',
+			component: ProfileEditView,
+		},
+		{
+			path: '/users',
+			name: 'Users',
+			component: UsersView
+		},
+		{
+			path: '/logout',
+			name: 'Logout',
+			component: LogoutView
+		},
+	]
 })
 
 // Check if the user is logged in 
 const checkLogInState = async function () {
-  if (storeUser.state.isAuthenticated === false) {
-    await storeUser.dispatch("login");
-  }
+	if (storeUser.state.isAuthenticated === false) {
+		await storeUser.dispatch("login");
+	}
 };
 
 router.beforeEach(async (to) => {
-  // if (to.name !== "UnAuthorized" && to.name !== "2fAuthenticate") {
-  // if (
-  //   to.name !== "UnAuthorized" &&
-  //   to.name !== "2fAuthenticate" &&
-  //   to.name !== "MatchHistoryView" //TODO this to be removed
-  // ) {
-  //   await checkLogInState();
-  //   if (to.name !== "Home" && storeUser.state.isAuthenticated === false) {
-  //     router.push({ name: "Home" });
-  //   }
-  // }
-  await checkLogInState();
-  if (to.name !== "Home" && storeUser.state.isAuthenticated === false) {
-    router.push({ name: "Home" });
-  }
+	// if (to.name !== "UnAuthorized" && to.name !== "2fAuthenticate") {
+	// if (
+	//   to.name !== "UnAuthorized" &&
+	//   to.name !== "2fAuthenticate" &&
+	//   to.name !== "MatchHistoryView" //TODO this to be removed
+	// ) {
+	//   await checkLogInState();
+	//   if (to.name !== "Home" && storeUser.state.isAuthenticated === false) {
+	//     router.push({ name: "Home" });
+	//   }
+	// }
+	await checkLogInState();
+	if (to.name !== "Home" && storeUser.state.isAuthenticated === false) {
+		router.push({ name: "Home" });
+	}
 });
 
 export default router
