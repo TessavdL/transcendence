ba9f66efd1a6249c4b48538690363d06eab23379
Tessa Van der loo <tevan-de@f1r2s6.codam.nl>
2023-03-31 17:00:37 +0200

    #76 updated UserMessage to have relationship with User and updated code in chatService so we can now access the user information through the relationship

--- back_end/app/src/chat/types.ts
+++ back_end/app/src/chat/types.ts
@@ -1,12 +1,14 @@
-export interface Messages {
-	intraId: number;
-	name: string;
-	text: string;
-}
-
 export interface Member {
 	intraId: number;
 	name: string;
 	avatar: string;
 	role: "OWNER" | "ADMIN" | "MEMBER";
+}
+
+export interface Message {
+	channelName: string;
+	intraId: number;
+	name: string;
+	avatar: string;
+	text: string;
 }
