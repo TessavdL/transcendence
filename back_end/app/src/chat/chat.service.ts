7df7a88037f166fea430c771d9881bf93f0c2bf9
TessavdL <tessavdl331@gmail.com>
2023-04-28 14:59:43 +0200

    #95 refactor: changed schema, removed name from relations because its not needed

--- back_end/app/src/chat/chat.service.ts
+++ back_end/app/src/chat/chat.service.ts
@@ -316,7 +316,7 @@ export class ChatService {
 
 	async getMessages(channelName: string): Promise<Message[]> {
 		try {
-			const channel: Channel & { userMessages: (UserMessage & { user: { intraId: number; intraName: string; avatar: string; }; })[]; } = await this.prisma.channel.findUnique({
+			const channel: Channel & { userMessages: (UserMessage & { user: { intraId: number; name: string; avatar: string; }; })[]; } = await this.prisma.channel.findUnique({
 				where: {
 					channelName: channelName,
 				},
@@ -326,7 +326,7 @@ export class ChatService {
 							user: {
 								select: {
 									intraId: true,
-									intraName: true,
+									name: true,
 									avatar: true,
 								},
 							},
@@ -337,8 +337,8 @@ export class ChatService {
 
 			const messages: Message[] = channel.userMessages.map((mes) => ({
 				channelName: channelName,
-				intraId: mes.user.intraId,
-				name: mes.user.intraName,
+				intraId: mes.intraId,
+				name: mes.user.name,
 				avatar: mes.user.avatar,
 				text: mes.text,
 			}));
@@ -381,7 +381,7 @@ export class ChatService {
 				filteredMessages.push({
 					channelName: channelName,
 					intraId: message.intraId,
-					name: message.name,
+					name: message.user.name,
 					avatar: message.user.avatar,
 					text: message.text,
 				});
