1ebb87ce854b3c348164fa684a5b978286d0e33c
Lisanne Houkes <lhoukes@f1r6s7.codam.nl>
2023-05-11 16:28:32 +0200

    ball movement to backend

--- back_end/app/src/game/game.service.ts
+++ back_end/app/src/game/game.service.ts
@@ -1,19 +1,72 @@
 import { Injectable } from '@nestjs/common';
+import { Game } from './type';
 
 @Injectable()
 export class GameService {
+	gameData(): Game {
+		const game: Game = {
+			player1Position: 240,
+			player2Position: 240,
+			ballPosition: { top: 300, left: 385 },
+			ballVelocity: { x: 5, y: 5 },
+			gameStarted: false,
+			player1Score: 0,
+			player2Score: 0,
+		};
+		return (game);
+	}
 	movement(movement: string): number {
 		if (movement ==='up') {
-			console.log(-25);
 			return (-25);
 		}
 		else if (movement === 'down') {
-			console.log(25);
 			return (25);
 		}
 		else {
-			console.log('invalid');
 			return (0);
 		}
 	}
+	ballMovement(gameStatus: Game): Game {
+		const ballPosition = {
+			top: gameStatus.ballPosition.top + gameStatus.ballVelocity.y,
+			left: gameStatus.ballPosition.left + gameStatus.ballVelocity.x,
+		};
+		// Check for collision with top or bottom walls
+		if (ballPosition.top <= 0 || ballPosition.top >= (600 - 20)) {
+			gameStatus.ballVelocity.y = -gameStatus.ballVelocity.y;
+		}
+		// Check for collision with left or right walls
+		if (ballPosition.left <= 0 || ballPosition.left >= (800 - 20)) {
+			gameStatus.ballVelocity.x = -gameStatus.ballVelocity.x;
+		}
+		// Check for score
+		if (ballPosition.left <= 0 ) {
+			gameStatus.ballPosition = { top: 300, left: 150 };
+			gameStatus.ballVelocity = { x: 5, y: 5 };
+			gameStatus.player2Score++;
+			gameStatus.gameStarted = false;
+		} 
+		else if (ballPosition.left >= 780) {
+			gameStatus.ballPosition = { top: 300, left: 650 };
+			gameStatus.ballVelocity = { x: -5, y: -5 };
+			gameStatus.player1Score++;
+			gameStatus.gameStarted = false;
+		} 
+		else {
+			gameStatus.ballPosition = ballPosition;
+		}
+		// Check for collision with player1 paddle
+		const paddleLeft = 0;
+		const paddleRight = 15;
+		const paddleTop = gameStatus.player1Position;
+		const paddleBottom = gameStatus.player1Position + 80;
+		if (ballPosition.left <= paddleRight + 15 && 
+		ballPosition.left >= paddleLeft &&
+		ballPosition.top + 20 >= paddleTop && 
+		ballPosition.top <= paddleBottom) {
+			gameStatus.ballVelocity.x = -gameStatus.ballVelocity.x;
+		}
+		return (gameStatus);
+	}
+	
 }
--- front_end/app/src/views/GameView.vue
+++ front_end/app/src/views/GameView.vue
@@ -1,48 +1,53 @@
 <template>
-	<div class="pong-game">
+	<div v-if="game" class="pong-game">
 		<div class="start-button-container">
-		<button @click="toggleGame">{{ gameStarted ? 'Stop' : 'Start' }}</button>
+		<button @click="toggleGame">{{ game.gameStarted ? 'Stop' : 'Start' }}</button>
 	</div>
 	<div class="scoreboard">
-		<div class="score-player1">Player 1: <span id="player-1-score">{{ player1Score }}</span></div>
-		<div class="score-player2">Player 2: <span id="player-2-score">{{ player2Score }}</span></div>
+		<div class="score-player1">Player 1: <span id="player-1-score">{{ game.player1Score }}</span></div>
+		<div class="score-player2">Player 2: <span id="player-2-score">{{ game.player2Score }}</span></div>
 	</div>
-	<div class="player1-paddle" :style="{ top: player1Position + 'px' }"></div>
-	<div class="player2-paddle" :style="{ top: player2Position + 'px' }"></div>
-	<div class="ball" :style="{ top: ballPosition.top + 'px', left: ballPosition.left + 'px' }"></div>
+	<div class="player1-paddle" :style="{ top: game.player1Position + 'px' }"></div>
+	<div class="player2-paddle" :style="{ top: game.player2Position + 'px' }"></div>
+	<div class="ball" :style="{ top: game.ballPosition.top + 'px', left: game.ballPosition.left + 'px' }"></div>
 	</div>
 </template>
 
 <script lang="ts">
 	import io from 'socket.io-client';
+	import type { Game } from '../types/GameType';
+import { computed, ref } from 'vue';
 
 	export default {
-		data() {
+	data() {
 		return {
-		player1Position: 240,
-		player2Position: 240,
-		ballPosition: { top: 300, left: 385 },
-		ballVelocity: { x: 5, y: 5 },
-		gameStarted: false,
-		player1Score: 0,
-        player2Score: 0,
+			game: ref<Game>
 		};
 	},
+
 	setup() {
 		const socket = io('http://localhost:3001/pong-game', { withCredentials: true });
-		return { socket };
+		const game = ref<Game>();
+		socket.on('gameData', (gameObject: Game) => {
+			game.value = gameObject;
+		});
+		return { socket, game: computed(() => game.value) };
 	},
+
 	mounted() {
-		this.socket.on('updateGameState', (gameState) => {
-			this.player1Position = gameState.player1Position;
-			this.player2Position = gameState.player2Position;
-			this.ballPosition = gameState.ballPosition;
-			this.player1Score = gameState.player1Score;
-			this.player2Score = gameState.player2Score;
+		this.socket.on('updategameStatus', (gameStatus) => {
+			this.game.player1Position = gameStatus.player1Position;
+			this.game.player2Position = gameStatus.player2Position;
+			this.game.ballPosition = gameStatus.ballPosition;
+			this.game.player1Score = gameStatus.player1Score;
+			this.game.player2Score = gameStatus.player2Score;
 		});
 		this.socket.on('updatePaddlePosition', (position) => {
 			this.movePaddle(position);
 		});
+		// this.socket.on('updateBallPosition', (ballPosition) => {
+		// 	this.update(ballPosition);
+		// });
 		window.addEventListener('keydown', (event) => {
 			if (event.key === 'ArrowUp') {
 				//this.movePaddle(-25); // move the paddle up by .. pixels
@@ -53,69 +58,88 @@
 			}
 		});
 	},
+
+	beforeRouteLeave() {
+		this.socket.disconnect();
+	},
+
 	methods: {
+
 		toggleGame() {
-		if (this.gameStarted) {
-			this.gameStarted = false;
+		if (this.game.gameStarted) {
+			this.game.gameStarted = false;
 		} else {
-			this.gameStarted = true;
+			this.game.gameStarted = true;
 		  this.update(); // call the update method to start the game loop
 		}
 	},
-		movePaddle(position) {
-		const newPosition = this.player1Position + position;
+		movePaddle(position: number) {
+		const newPosition = this.game.player1Position + position;
 		if (newPosition <= 500 && newPosition >= 0) {
 			//this.socket.emit('movePaddle', position);
-			this.player1Position = newPosition;
+			this.game.player1Position = newPosition;
 		}
 		console.log('position player 1:', newPosition);
 	},
 		update() {
-		if (!this.gameStarted)
-			return;
-		const ballPosition = {
-			top: this.ballPosition.top + this.ballVelocity.y,
-			left: this.ballPosition.left + this.ballVelocity.x,
-		};
-
-		// Check for collision with top or bottom walls
-		if (ballPosition.top <= 0 || ballPosition.top >= (600 - 20)) {
-			this.ballVelocity.y = -this.ballVelocity.y;
-		}
-		// Check for collision with left or right walls
-		if (ballPosition.left <= 0 || ballPosition.left >= (800 - 20)) {
-			this.ballVelocity.x = -this.ballVelocity.x;
-		}
-		// Check for score
-		if (ballPosition.left <= 0 ) {
-			this.ballPosition = { top: 300, left: 150 };
-			this.ballVelocity = { x: 5, y: 5 };
-			this.player2Score++;
-			this.gameStarted = false;
-		} 
-		else if (ballPosition.left >= 780) {
-			this.ballPosition = { top: 300, left: 650 };
-			this.ballVelocity = { x: -5, y: -5 };
-			this.player1Score++;
-			this.gameStarted = false;
-		} 
-		else {
-			this.ballPosition = ballPosition;
-		}
-		// Check for collision with player1 paddle
-		const paddleLeft = 0;
-		const paddleRight = 15;
-		const paddleTop = this.player1Position;
-		const paddleBottom = this.player1Position + 80;
-		if (ballPosition.left <= paddleRight + 15 && 
-		ballPosition.left >= paddleLeft &&
-		ballPosition.top + 20 >= paddleTop && 
-		ballPosition.top <= paddleBottom) {
-			this.ballVelocity.x = -this.ballVelocity.x;
-		}
-		this.socket.emit('updateBallPosition', ballPosition);
-		console.log('Ball position:', ballPosition);
+			if (!this.game.gameStarted)
+				return;
+			const gameStatus = {
+				ballPosition: this.game.ballPosition,
+				ballVelocity: this.game.ballVelocity,
+				player1Position: this.game.player1Position,
+				player2Position: this.game.player2Position,
+				player1Score: this.game.player1Score,
+				player2Score: this.game.player2Score,
+				gameStarted: this.game.gameStarted,
+			};
+		this.socket.emit('ballMovement', gameStatus);
 		requestAnimationFrame(this.update);
+		// if (!this.game.gameStarted)
+		// 	return;
+		// const ballPosition = {
+		// 	top: this.game.ballPosition.top + this.game.ballVelocity.y,
+		// 	left: this.game.ballPosition.left + this.game.ballVelocity.x,
+		// };
+
+		// // Check for collision with top or bottom walls
+		// if (ballPosition.top <= 0 || ballPosition.top >= (600 - 20)) {
+		// 	this.game.ballVelocity.y = -this.game.ballVelocity.y;
+		// }
+		// // Check for collision with left or right walls
+		// if (ballPosition.left <= 0 || ballPosition.left >= (800 - 20)) {
+		// 	this.game.ballVelocity.x = -this.game.ballVelocity.x;
+		// }
+		// // Check for score
+		// if (ballPosition.left <= 0 ) {
+		// 	this.game.ballPosition = { top: 300, left: 150 };
+		// 	this.game.ballVelocity = { x: 5, y: 5 };
+		// 	this.game.player2Score++;
+		// 	this.game.gameStarted = false;
+		// } 
+		// else if (ballPosition.left >= 780) {
+		// 	this.game.ballPosition = { top: 300, left: 650 };
+		// 	this.game.ballVelocity = { x: -5, y: -5 };
+		// 	this.game.player1Score++;
+		// 	this.game.gameStarted = false;
+		// } 
+		// else {
+		// 	this.game.ballPosition = ballPosition;
+		// }
+		// // Check for collision with player1 paddle
+		// const paddleLeft = 0;
+		// const paddleRight = 15;
+		// const paddleTop = this.game.player1Position;
+		// const paddleBottom = this.game.player1Position + 80;
+		// if (ballPosition.left <= paddleRight + 15 && 
+		// ballPosition.left >= paddleLeft &&
+		// ballPosition.top + 20 >= paddleTop && 
+		// ballPosition.top <= paddleBottom) {
+		// 	this.game.ballVelocity.x = -this.game.ballVelocity.x;
+		// }
+		// this.socket.emit('updateBallPosition', ballPosition);
+		// console.log('Ball position:', ballPosition);
+		// requestAnimationFrame(this.update);
 		},
 	},
 };
@@ -149,8 +173,8 @@
 	border-right: 8px solid rgb(253, 251, 251);
 }
 @font-face {
-  font-family: "arcadeFont";
-  src: url("./src/assets/game_images/ARCADECLASSIC.TTF");
+	font-family: "arcadeFont";
+	src: url("./src/assets/game_images/ARCADECLASSIC.TTF");
 }
 
 .scoreboard {
